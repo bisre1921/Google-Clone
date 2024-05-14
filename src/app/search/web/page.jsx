a65de25bd67f21a95172e627ac81b5c30ae4b7b4
bisre1921 <bisrattewodros3@gmail.com>
2024-05-14 13:51:52 +0300

    fetch data from google search api and shows the titles

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,6 +1,19 @@
-const WebSearchPage = () => {
+const WebSearchPage = async ({searchParams}) => {
+    const response = await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+    );
+    const data = await response.json();
+    const results = data.items;
     return (
-        <div>web search</div>
+        <div>
+            {results && 
+                results.map(result => 
+                    <h1>
+                        {result.title}
+                    </h1>
+                )
+            }
+        </div>
     )
 };
 
