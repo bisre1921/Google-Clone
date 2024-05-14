faf4bd5082f35a586c0d0140eab37f3f5fb6b3a4
bisre1921 <bisrattewodros3@gmail.com>
2024-05-14 15:01:16 +0300

    create search result component

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,6 +1,9 @@
+import WebSearchResult from "@/components/WebSearchResult";
 import Link from "next/link";
 
 const WebSearchPage = async ({searchParams}) => {
+    await new Promise((resolve) => setTimeout(resolve , 5000));
+
     const response = await fetch(
         `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
     );
@@ -25,11 +28,7 @@ const WebSearchPage = async ({searchParams}) => {
     return (
         <div>
             {results && 
-                results.map(result => 
-                    <h1>
-                        {result.title}
-                    </h1>
-                )
+                <WebSearchResult results={data} />
             }
         </div>
     )
